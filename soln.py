1d2bf2456ab9900c0f38bb88d4dbe33632e7d8e2
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-04-15 00:35:52 +0300

    Create soln.py

---
+++ soln.py
@@ -0,0 +1,7 @@
+# Create properties
+property1 = Property("123 Main St", "John Doe")
+property2 = Property("456 Oak St", "Jane Smith")
+
+# Create units
+unit1 = Unit(500, 10000, 20000)
+unit2 = Unit(750, 15000, 300
