3295f73417270faf3546cbd07c349640b45fb939
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-04-21 23:16:13 +0300

    Update index.php

--- PMS/index.php
+++ PMS/index.php
@@ -1,1 +1,46 @@
-<!doctype>
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "property_management_system";
+
+// Connecting databse with  the server
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// This will get thed properties of PMS in the databasee 
+
+$sql = "SELECT * FROM properties";
+$result = $conn->query($sql);
+$properties = array();
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $properties[] = $row;
+  }
+}
+
+// Close server connection
+$conn->close();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Property Management System</title>
+</head>
+<body>
+  <h1>Properties</h1>
+  <?php foreach ($properties as $property): ?>
+    <h2><?= $property['name'] ?></h2>
+    <p><?= $property['address'] ?></p>
+    <p><?= $property['owner_name'] ?></p>
+    <a href="view_property.php?id=<?= $property['id'] ?>">View Property</a>
+  <?php endforeach ?>
+</body>
+</html>
