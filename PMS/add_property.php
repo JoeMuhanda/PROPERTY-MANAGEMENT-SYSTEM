c11416b860823c2f1d6702f5310f18ed7901e9e1
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-04-28 23:01:34 +0300

    Update add_property.php

--- PMS/add_property.php
+++ PMS/add_property.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $password = "password";
   $dbname = "property_management_system";
 
-  // Create connectionreate connectionreate 
+  // Create connectionreate connectio
   $conn = new mysqli($servername, $username, $password, $dbname);
 
   // Check connection
