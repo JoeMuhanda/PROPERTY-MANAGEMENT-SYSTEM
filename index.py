111505e826ce59576144bbe491e9954f0a02be3c
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-04-28 22:58:43 +0300

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ class Property:
     def __init__(self, address, owner):
         self.address = address
         self.owner = owner
-        self.units = []maq
+        self.units = []ma
 
     def add_unit(self, unit):
         self.units.append(unit)
